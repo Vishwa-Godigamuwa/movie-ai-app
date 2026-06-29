41b48b5ec52463cba77382258db3cc7f54f00dd4
Conqueror-Shanks <vishwagodigamuwa@gmail.com>
2026-06-29 23:30:25 +0530

    Connected MongoDB Atlas to backend

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
 
 const app = express();
 
-// middleware
 app.use(express.json());
 
-// test route
 app.get("/", (req, res) => {
   res.send("🚀 Movie AI Backend Running");
 });
 
-// start server
-app.listen(5000, () => {
-  console.log("✅ Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
